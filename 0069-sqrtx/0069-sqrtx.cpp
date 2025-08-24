76dcad0be32e1963d999aa920e902fc979cfc9eb
Aditii-12 <145634635+Aditii-12@users.noreply.github.com>
2025-08-24 15:11:22 +0530

    Time: 0 ms (100%), Space: 8.5 MB (50.51%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x<2) return x;
+        int l=1;
+        int h=x/2;
+        while(l<=h){
+            int mid=(l+h)/2;
+            long long sq=(long long) mid*mid;
+            if(sq==x) return mid;
+            else if(sq<x) l=mid+1;
+            else h=mid-1;
+        }
+        return h;
+    }
+};
