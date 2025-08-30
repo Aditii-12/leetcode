8cf60114475974182a36d233ba85168e5300ffc0
Aditii-12 <145634635+Aditii-12@users.noreply.github.com>
2025-08-30 15:42:33 +0530

    Time: 2 ms (85.38%), Space: 131.5 MB (62.54%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -4,17 +4,18 @@ public:
         int maxele=*max_element(nums.begin(),nums.end());
         int l=0,r=0;
         int n=nums.size();
-        long long loba=0;
-        int count=0;
+        long long ans=0;
+        int freq=0;
         while(r<n){
-            if(nums[r]==maxele) count++;
-            while(count>=k){
-                if(nums[l]==maxele) count--;
+            if(nums[r]==maxele) freq++;
+            while(freq>=k){
+                ans+=(n-r);
+                if(nums[l]==maxele) freq--;
                 l++;
             }
-            loba+=l;
             r++;
         }
-        return loba;
+        return ans;
+
     }
 };
