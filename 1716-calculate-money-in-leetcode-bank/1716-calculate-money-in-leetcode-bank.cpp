e86d3729bd77d5d15a0ed91b159deeb02fe6f74d
Aditii-12 <145634635+Aditii-12@users.noreply.github.com>
2025-10-25 23:43:27 +0530

    Time: 0 ms (100%), Space: 7.8 MB (53.24%) - LeetHub

---
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int totalMoney(int n) {
+        int ans=0;
+        int m=1;
+        
+        while (n>0) {
+            for(int i=0;i<min(n,7);i++) {
+                ans+=m+i;
+            }
+            n-=7;
+            m++;
+        }
+        
+        return ans;
+    }
+};
