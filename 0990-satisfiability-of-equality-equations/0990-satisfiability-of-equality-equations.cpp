2a7592db2335e8bec621d34304c280dab17fc613
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2025-10-31 14:32:31 +0530

    Time: 0 ms (100%), Space: 15.1 MB (52.22%) - LeetHub

--- 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.cpp
+++ 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.cpp
@@ -1,48 +1,41 @@
 class Solution {
 public:
-    vector<int>parent;
-    vector<int> rank;
-    int find(int x){
+vector<int>parent;
+vector<int>rank;
+    int find(int x){ //for finding the parent of x
         if(parent[x]==x) return x;
         else
             return parent[x]=find(parent[x]);
     }
     void unionn(int x,int y){
-        int x_p=find(x);
-        int y_p=find(y);
-        if(x_p == y_p) return;
-        if(rank[x_p]>rank[y_p]){
-            parent[y_p]=x_p;
-        }
-        else if(rank[x_p]<rank[y_p]){
-            parent[x_p]=y_p;
-        }
+        int x_par=find(x);
+        int y_par=find(y);
+        if(x_par==y_par) return;
+        if(rank[x_par]>rank[y_par]) parent[y_par]=x_par;
+        else if(rank[x_par]<rank[y_par]) parent[x_par]=y_par;
         else {
-            parent[x_p]=y_p;
-            rank[y_p]++;
+            rank[x_par]++;
+            parent[y_par]=x_par;
         }
     }
     bool equationsPossible(vector<string>& equations) {
         int n=equations.size();
-        parent.resize(26);
+        parent.resize(26,0);
         rank.resize(26,0);
-        for(int i=0;i<26;i++){
-            parent[i]=i;
+        for(int i=0;i<26;i++) parent[i]=i;
+        for(auto it:equations){
+            if(it[1]=='=') //mtlb equal h dono
+                unionn(it[0]-'a',it[3]-'a');
+        }
+        for(auto it:equations){
+            if(it[1]=='!'){
+                int x_par=find(it[0]-'a');
+                int y_par=find(it[3]-'a');
+                if(x_par==y_par) return 0;
+            }
         }
 
-        for(auto s:equations){
-            if(s[1]=='=') { //means two char are equal to unka union
-            unionn(s[0]-'a',s[3]-'a');
-            }
-        }
-        //find not equal wale case and validate
-        for(auto s:equations){
-            if(s[1]=='!'){
-                int p1=find(s[0]-'a');
-                int p2=find(s[3]-'a');
-                if(p1==p2) return false;
-            }
-        }
-        return true;
+        return 1;
+        
     }
 };
