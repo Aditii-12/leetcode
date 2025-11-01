9a2376ace59e06eabcda3810637320bea64d487f
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2025-11-01 13:52:18 +0530

    Time: 59 ms (26.72%), Space: 88.6 MB (15.24%) - LeetHub

--- 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
     bool func(int mid,vector<int>& quantities,int n){
-        int j=0;
-        int rem=quantities[j];
-        //max we can give to a store is mid
-        for(int i=0;i<n;i++){
-            if(rem<=mid){
-                j++;
-                if(j==quantities.size()) return 1; 
-                //end me aagye and sare baat diya 
-                else{
-                    rem=quantities[j]; //set to next product
-                }
-            }
-            else{
-                rem-=mid;
-            }
+        // int j=0;
+        // int rem=quantities[j];
+        // //max we can give to a store is mid
+        // for(int i=0;i<n;i++){
+        //     if(rem<=mid){
+        //         j++;
+        //         if(j==quantities.size()) return 1; 
+        //         //end me aagye and sare baat diya 
+        //         else{
+        //             rem=quantities[j]; //set to next product
+        //         }
+        //     }
+        //     else{
+        //         rem-=mid;
+        //     }
+        // }
+        // return 0;
+
+        //SECOND APPROACH IS(OTIMIZED ONE)
+        //IS THAT FOR A MAX QUANITY WHICH IS PASSED IN A FUCNTION
+        //WE CALCULATE NUMBER OF STORES WE NEED
+        //IF THAT<=N RETURN 1;
+        //ELSE RETURN 0
+        if(mid==0) return 0;
+        int temp=0;
+        for(int i=0;i<quantities.size();i++){
+            temp+=ceil((double)quantities[i]/mid);
         }
-        return 0;
+        return temp<=n;
     }
     int minimizedMaximum(int n, vector<int>& quantities) {
         int l=1;
