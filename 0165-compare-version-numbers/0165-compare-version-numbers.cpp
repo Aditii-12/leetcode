a572687e5702451a26158a4669a62aa146810503
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-01-05 15:28:37 +0530

    Time: 0 ms (100%), Space: 8.1 MB (83.05%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int compareVersion(string v1, string v2) {
+        int n=v1.length();
+        int m=v2.length();
+        int i=0;
+        int j=0;
+        while(i<n||j<m){
+            int num1=0;
+            int num2=0;
+            while(i<n && v1[i]!='.'){
+                num1=num1*10+(v1[i]-'0');
+                i++;
+            }
+            while(j<m && v2[j]!='.'){
+                num2=num2*10+(v2[j]-'0');
+                j++;
+            }
+            if(num1>num2) return 1;
+            if(num2>num1) return -1;
+            i++;
+            j++;
+            
+        }
+        return 0;
+        
+    }
+};
