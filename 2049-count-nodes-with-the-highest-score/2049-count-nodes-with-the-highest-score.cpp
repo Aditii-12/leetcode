e4f48eb621661aafe272342f682d5faf74200a37
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-01-07 13:21:27 +0530

    Time: 138 ms (57.83%), Space: 115.7 MB (85.3%) - LeetHub

---
+++ 2049-count-nodes-with-the-highest-score/2049-count-nodes-with-the-highest-score.cpp
@@ -0,0 +1,36 @@
+using ll=long long;
+class Solution {
+public:
+    ll maxi=0; //max score
+    int cnt=0;
+    int n;
+    vector<vector<int>>tree;
+    ll dfs(int u){
+        ll score=1; //isme we will multiply
+        ll sz=0; //child size(left and right alg se)
+
+        for(int v:tree[u]){
+            ll childsz=dfs(v);
+            score*=childsz;
+            sz+=childsz; //to find the 
+        }
+        ll rest=n-sz-1;
+        if(rest>0){
+            score*=rest;
+        }
+        if(score>maxi) {maxi=score; 
+            cnt=1;
+        }
+        else if(score==maxi) cnt++;
+        return sz+1;
+    }
+    int countHighestScoreNodes(vector<int>& parents) {
+        n=parents.size(); //no of nodesin tree
+        tree.assign(n,{});
+        for(int i=1;i<n;i++){
+            tree[parents[i]].push_back(i);
+        }
+        dfs(0);
+        return cnt;
+    }
+};
