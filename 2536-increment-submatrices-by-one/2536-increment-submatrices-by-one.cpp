c02ecf51079395b2b7ff41a5c11b3c5198a22658
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-01-17 16:27:37 +0530

    Time: 54 ms (41.55%), Space: 99.2 MB (21.45%) - LeetHub

--- 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.cpp
+++ 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
-        vector diff(n + 1, vector<int>(n + 1));
-        for (const auto& query : queries) {
-            int row1 = query[0], col1 = query[1];
-            int row2 = query[2], col2 = query[3];
-            diff[row1][col1] += 1;
-            diff[row2 + 1][col1] -= 1;
-            diff[row1][col2 + 1] -= 1;
-            diff[row2 + 1][col2 + 1] += 1;
-        }
-        vector mat(n, vector<int>(n));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                int x1 = (i == 0) ? 0 : mat[i - 1][j];
-                int x2 = (j == 0) ? 0 : mat[i][j - 1];
-                int x3 = (i == 0 || j == 0) ? 0 : mat[i - 1][j - 1];
-                mat[i][j] = diff[i][j] + x1 + x2 - x3;
+        vector<vector<int>>ans(n,vector<int>(n,0)); //n*n matrix me 0 bhr diya
+        for(auto &it:queries){
+            int r1=it[0];
+            int c1=it[1];
+            int r2=it[2];
+            int c2=it[3];
+            for(int i=r1;i<=r2;i++){
+                ans[i][c1]+=1;
+                if(c2+1<n)
+                    ans[i][c2+1]--;
             }
         }
-        return mat;
+        for(int i=0;i<n;i++){
+            for(int j=1;j<n;j++){
+                ans[i][j]+=ans[i][j-1];
+            }
+        }
+        return ans;
     }
 };
