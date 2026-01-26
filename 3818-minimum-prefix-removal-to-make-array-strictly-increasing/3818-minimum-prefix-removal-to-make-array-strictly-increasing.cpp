e0608c9c72787c797b0408cd77d051f57155b0c8
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-01-26 14:26:37 +0530

    Time: 16 ms (0%), Space: 194.8 MB (0%) - LeetHub

--- 3818-minimum-prefix-removal-to-make-array-strictly-increasing/3818-minimum-prefix-removal-to-make-array-strictly-increasing.cpp
+++ 3818-minimum-prefix-removal-to-make-array-strictly-increasing/3818-minimum-prefix-removal-to-make-array-strictly-increasing.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
-    bool check(vector<int>& nums, int i){
-        for(int j=i+1;j<nums.size()-1;j++){
-            if(nums[j]>=nums[j+1]) return 0; //not strictly increasing
-        }
-        return 1;
-    }
     int minimumPrefixLength(vector<int>& nums) {
         int n=nums.size();
-        if(check(nums,-1)) return 0;
-        for(int i=0;i<n-1;i++){
-            if(check(nums,i)) return i+1;
+        vector<bool>f(n,0);
+        f[n-1]=1; //last element alswways strcitly increqsing
+        //basically f will store kaha se array strcitly increasing h
+
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]&&f[i+1])
+                f[i]=1;
+            else
+                f[i]=0;
         }
+        for(int i=-1;i<n-1;i++){
+            if(f[i+1])
+                return i+1;
+        }
+
         return n;
     }
 };
