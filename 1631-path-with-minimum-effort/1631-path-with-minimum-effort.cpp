d5ea373373fba8942db5a9200def1e323d751a4c
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-01-28 17:36:20 +0530

    Time: 51 ms (66.52%), Space: 24.3 MB (91.37%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
+int dr[4]={0,-1,1,0};
+int dc[4]={-1,0,0,1};
     int minimumEffortPath(vector<vector<int>>& heights) {
+        int n=heights.size();
+        int m=heights[0].size();
         priority_queue<pair<int, pair<int, int>>, //diff,row,col
                        vector<pair<int, pair<int, int>>>,
                        greater<pair<int, pair<int, int>>>>pq;
-        int n=heights.size();
-        int m=heights[0].size();
         vector<vector<int>> dis(n,vector<int>(m,1e9)); //everything marked as infinite
         dis[0][0]=0;
         pq.push({0,{0,0}});
-        int dr[]={0,-1,1,0};
-        int dc[]={-1,0,0,1};
         while(!pq.empty()){
             auto it=pq.top();
             pq.pop();
-            int diff=it.first;
+            int dif=it.first;
             int row=it.second.first;
             int col=it.second.second;
-            if(row==n-1 && col==m-1){
-                return diff;
-            }
+            if(row==n-1&&col==m-1) return dif;
             for(int i=0;i<4;i++){
                 int nr=row+dr[i];
                 int nc=col+dc[i];
-                if(nr>=0 && nr<n && nc>=0 && nc<m){
-                    int effort=max(abs(heights[row][col]-heights[nr][nc]),diff);
-                    if(effort<dis[nr][nc]){
-                        dis[nr][nc]=effort;
-                        pq.push({effort,{nr,nc}});
-                    }
+                if(nr<0||nr>=n||nc<0||nc>=m) continue;
+                int temp=max(abs(heights[row][col]-heights[nr][nc]),dif);
+                if(temp<dis[nr][nc]){
+                    dis[nr][nc]=temp;
+                    pq.push({temp,{nr,nc}});
                 }
             }
-
         }
-        return 0; 
+        return 0;
     }
 };
