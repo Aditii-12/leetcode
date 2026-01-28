691832b5a45419a330f913c7ca6bfa431f3de9ef
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-01-28 16:43:32 +0530

    Time: 103 ms (10.96%), Space: 47.8 MB (32.12%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,38 +1,35 @@
-//hm bs dijksrtra algo use krenge
-//and usme sbse distance nikalkr
-//jo max distance hogi among all
-//vhi return
-//because that will be min dist possible to cover all nodes
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        unordered_map<int, vector<pair<int, int>>> adj;
-        for(auto &vec : times) {
-            int u = vec[0];
-            int v = vec[1];
-            int w = vec[2];
-            adj[u].push_back({v, w});
+        unordered_map<int,vector<pair<int,int>>>adj;
+        for(auto it:times){
+            int u=it[0];
+            int v=it[1];
+            int w=it[2];
+            adj[u].push_back({v,w});
         }
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-	vector<int> result(n+1, INT_MAX);
-	result[k] = 0;
-	pq.push({0, k});
-	while(!pq.empty()) {
-	    int d  = pq.top().first;
-	    int node = pq.top().second;
-	    pq.pop();
-	    for(auto &vec : adj[node]) {
-		int node= vec.first;
-		int dist= vec.second;
-		if(d + dist < result[node]) {
-		    result[node] = d + dist;
-		    pq.push({d+dist, node});
-		}
-	    }
-	}
+        vector<int> result(n+1, INT_MAX);
+        result[k] = 0;
+	    pq.push({0, k}); 
+        //we r pushing cost and then node
+        while(!pq.empty()){
+            int d  = pq.top().first;
+	        int node = pq.top().second;
+	        pq.pop();
+            if (d > result[node]) continue;
+            for(auto &vec : adj[node]) {
+		    int it= vec.first; //it is node
+		    int dist= vec.second;
+		        if(d + dist < result[it]) {
+		            result[it] = d + dist;
+		            pq.push({d+dist,it});
+		        }
+	        }
+        }
         int ans = INT_MIN;
         for(int i = 1; i <= n; i++)
             ans = max(ans, result[i]);
-	return ans == INT_MAX ? -1 : ans;
+	    return ans==INT_MAX?-1:ans;
     }
 };
