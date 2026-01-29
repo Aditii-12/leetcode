5a0d0c4b3e2d9e6b021b86a789d28e19bd60da54
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-01-29 14:13:51 +0530

    Time: 73 ms (22.4%), Space: 107.1 MB (24.25%) - LeetHub

---
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void dijk(int src,vector<pair<int,int>>adj[],vector<int>& dist){
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        dist[src] = 0;
+        pq.push({0,src});
+        while(!pq.empty()){
+            auto[d,u]=pq.top();
+            pq.pop();
+            if(d<dist[u]) continue;
+            for(auto [v,w]:adj[u]){
+                if(dist[v]>d+w){
+                    dist[v]=d+w;
+                    pq.push({dist[v],v});
+                }
+            }
+        }
+    }
+    long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
+        if(source.size()!=target.size()) return -1;
+        vector<pair<int,int>>adj[26];
+        for(int i=0;i<original.size();i++){
+            adj[original[i]-'a'].push_back({changed[i]-'a',cost[i]});
+        }
+        vector<vector<int>> dist(26,vector<int>(26,INT_MAX));
+        //har ek letter se har ek letter ki shortest distnce we will store
+        for(int i=0;i<26;i++){
+            dijk(i,adj,dist[i]);
+        }
+        long long ans=0;
+        for(int i=0;i<source.size();i++){
+            int u=source[i]-'a';
+            int v=target[i]-'a';
+            if(u==v) continue;
+            if(dist[u][v]==INT_MAX) return -1;
+            ans+=dist[u][v];
+        }
+        return ans;
+    }
+};
