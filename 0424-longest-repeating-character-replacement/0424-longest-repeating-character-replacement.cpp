53c99541ccacaf72d81328e3582d066115ae2c81
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-03-10 10:44:33 +0530

    Time: 9 ms (33.07%), Space: 11 MB (18.37%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
+        int maxi=0;
         int n=s.length();
         int l=0,r=0;
-        int ans=0;
-        int maxf=0;
+        int maxif=0;
         map<char,int>mpp;
         while(r<n){
             mpp[s[r]]++;
-            maxf=max(maxf,mpp[s[r]]);
-            while((r-l+1)-maxf>k){
+            maxif=max(maxif,mpp[s[r]]);
+            while((r-l+1)-maxif>k){ //window size-maxif == char that needs to be replaced
                 mpp[s[l]]--;
                 l++;
             }
-            ans=max(ans,r-l+1);
+            maxi=max(maxi,r-l+1);
             r++;
         }
-        return ans;
+        return maxi;
     }
 };
