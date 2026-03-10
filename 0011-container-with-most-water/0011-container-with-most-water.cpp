f9d5e03c92e91213781b7e60a186e4d504699371
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-03-10 14:47:17 +0530

    Time: 0 ms (100%), Space: 62.9 MB (48.08%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
-    int maxArea(vector<int>& height) {
-        int n=height.size();
-        int i=0,j=n-1;
-        int ans=0;
-        while (i<j) {
-            ans=max(ans,(j-i)*min(height[i],height[j]));
-            if (height[i]<height[j]) i++;
-            else j--;
+    int maxArea(vector<int>& heights) {
+        int n=heights.size();
+        int l=0;
+        int r=n-1;
+        int ans=INT_MIN;
+        while(l<r){
+            ans=max(ans,min(heights[l],heights[r])*(r-l));
+            if(heights[l]<heights[r]) l++;
+            else r--; //kyuki ans is dependent on choti height 
+            //to choti height rkhke fayda ni h so pointer move krdenge
         }
         return ans;
     }
