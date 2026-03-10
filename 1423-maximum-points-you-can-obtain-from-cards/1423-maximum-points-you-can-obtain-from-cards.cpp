9a25b3b8cf19492ebee63049fb0590100a6e7484
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-03-10 15:44:32 +0530

    Time: 0 ms (100%), Space: 46.2 MB (63.63%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
-    int maxScore(vector<int>& ca, int k) {
-      int n=ca.size();
-      int lsum=0,rsum=0;
-      int r=n-1;
-      for(int i=0;i<k;i++){
-        lsum+=ca[i];
-      }
-      int msum=lsum;
-      for(int i=k-1;i>=0;i--){
-        lsum-=ca[i];
-        rsum+=ca[r];
-        r--;
-        msum=max(msum,lsum+rsum);
-      }
-      return msum;
-
+//finding window of size n-k with min sum
+//so left card will give max sum
+    int maxScore(vector<int>& c, int k) {
+        int n=c.size();
+        int win=n-k;
+        int s=accumulate(c.begin(),c.end(),0);
+        if(win==0) return s;
+        int sum=0;
+        for(int i=0;i<win;i++){
+            sum+=c[i];
+        }
+        int mini=sum;
+        int r=win;
+        int l=0;
+        while(r<n){
+            sum+=c[r];
+            sum-=c[l];
+            r++;
+            l++;
+            mini=min(mini,sum);
+        }
+        
+        return s-mini;
     }
 };
