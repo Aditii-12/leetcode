ec29149d134247bb234e5360f4ef34fab0e61e30
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-03-11 15:54:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,40 +1,37 @@
 class Solution {
 public:
-    int func(vector<int>&wt ,int mid){
-        int days=0;
-        int sum=0;
-        for(int i=0;i<wt.size();i++){
-            sum+=wt[i];
-            if(sum>mid){
-                days++;
-                sum=wt[i];
+    bool func(vector<int>& wts,int days,int mid){
+        int n=wts.size();
+        int temp=0;
+        int t=0;
+        for(int i=0;i<n;i++){
+            t+=wts[i];
+            if(t>mid){
+                temp++;
+                t=wts[i];
             }
-            else if(sum==mid){
-                days++;
-                sum=0;
+            else if(t==mid){
+                temp++;
+                t=0;
             }
         }
-        if(sum>0) days++;
-        return days;
+        if(t>0) temp++;
+        return temp<=days;
+
     }
-    int shipWithinDays(vector<int>& wt, int days) {
-        int n=wt.size();
-        int ans=-1;
-        int low=*max_element(wt.begin(),wt.end());
-        int high=accumulate(wt.begin(),wt.end(),0LL);
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(func(wt,mid)>days){
-                low=mid+1;
-                //weight bdhana pdega jo that kam din lge
-            }
-            else{
+    int shipWithinDays(vector<int>& wts, int days) {
+        int n=wts.size();
+        int lo=*max_element(wts.begin(),wts.end());
+        int hi=accumulate(wts.begin(),wts.end(),0);
+        int ans;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(func(wts,days,mid)){
                 ans=mid;
-                high=mid-1;
-                //weight kam krne ka try krenge
+                hi=mid-1;
             }
+            else lo=mid+1;
         }
         return ans;
-        
     }
 };
