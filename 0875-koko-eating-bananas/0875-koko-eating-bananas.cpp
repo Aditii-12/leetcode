9e17edf076d72c7374cb49e20f278e57368f8855
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-03-11 15:25:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    long long func(vector<int>& piles, int speed) {
-        long long hours = 0;
-        for (int i = 0; i < piles.size(); i++) {
-            hours += (piles[i] + speed - 1) / speed; // ceil division
+    bool func(int mid,vector<int>& piles, int h){
+        int n=piles.size();
+        int hours=0;
+        for(int i=0;i<n;i++){
+            hours+=ceil((double)piles[i]/mid);
         }
-        return hours;
+        return hours<=h;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
         int n=piles.size();
-        int low=1;
-        int high=*max_element(piles.begin(),piles.end());
-        int ans=high;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(func(piles,mid)>h) {
-                low=mid+1; //speed to bdhana pdega
-            }
-            else{
+        int lo=1;
+        int hi=*max_element(piles.begin(),piles.end());
+        int ans=-1;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(func(mid,piles,h)){
                 ans=mid;
-                high=mid-1;
+                hi=mid-1;
             }
+            else lo=mid+1;
         }
         return ans;
     }
