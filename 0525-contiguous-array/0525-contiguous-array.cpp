dc17d4b7eb5f73c1eba24e7cd51924544dd8f1e8
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-03-17 10:35:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -3,8 +3,8 @@ public:
     int findMaxLength(vector<int>& nums) {
         int n=nums.size();
         int cnt=0;
+        unordered_map<int,int>mpp;
         int ans=0;
-        map<int,int>mpp; //in this we store first occurence of cnt
         mpp[0]=-1;
         for(int i=0;i<n;i++){
             if(nums[i]==0) cnt--;
