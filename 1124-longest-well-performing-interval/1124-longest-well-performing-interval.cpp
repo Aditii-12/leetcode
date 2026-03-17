cd3b27f53f77d5e6abc0ac9f184f8f69b80cc8fe
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-03-17 16:22:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1124-longest-well-performing-interval/1124-longest-well-performing-interval.cpp
+++ 1124-longest-well-performing-interval/1124-longest-well-performing-interval.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-//somewhat similar question as contigous array 525
     int longestWPI(vector<int>& hours) {
         int n=hours.size();
-        int ans=0;
         int cnt=0;
-        map<int,int>mpp;
+        unordered_map<int,int>mpp;
+        int ans=0;
         mpp[0]=-1;
         for(int i=0;i<n;i++){
             if(hours[i]<=8) cnt--;
             else cnt++;
-            if(cnt>0) ans=i+1; //abi tk ka longest yhi hoga thats why we r not using max
+            if(cnt>0) ans=i+1;
             else{
-                if(mpp.find(cnt-1)!=mpp.end())
+                if(mpp.find(cnt-1)!=mpp.end()){
                     ans=max(ans,i-mpp[cnt-1]);
-                else mpp[cnt-1]=i; //store first occurence only
+                }
+                else mpp[cnt-1]=i;
             }
-
         }
         return ans;
+
     }
 };
