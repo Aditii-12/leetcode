5331ea3c7e24e3c1bdabe4d99f53e26562c1382c
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-03-17 09:33:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -3,17 +3,16 @@ public:
     bool checkSubarraySum(vector<int>& nums, int k) {
         int n=nums.size();
         unordered_map<int,int>mpp;
-        mpp[0]=-1; //we r putting that we found 0 at index -1
-        //so arrays starting at 0 counts
         int sum=0;
+        mpp[0]=-1;
         for(int i=0;i<n;i++){
-            sum+=nums[i]; //this we got prefix sum
-            int mod=sum%k;
-            if(mpp.find(mod)!=mpp.end()){ //already milgya
-                if(i-mpp[mod]>=2) return 1;
+            sum+=nums[i]; //prefix sum
+            int t=sum%k;
+            if(mpp.find(t)!=mpp.end()){
+                if(i-mpp[t]>=2) return 1;
             }
             else{
-                mpp[mod]=i; //pehli br mila h
+                mpp[t]=i;
             }
         }
         return 0;
