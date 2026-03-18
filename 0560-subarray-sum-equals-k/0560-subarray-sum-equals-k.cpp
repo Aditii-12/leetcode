93115821ff55bc409e3e3c27ec6f18d26dd39bf8
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-03-18 09:20:19 +0530

    Time: 41 ms (63.17%), Space: 45.4 MB (54.04%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -6,13 +6,13 @@ public:
         mpp[0]=1;
         int sum=0;
         int cnt=0;
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
-            if(mpp.find(sum-k)!=mpp.end())
+        for(int x:nums){
+            sum+=x;
+            if(mpp.find(sum-k)!=mpp.end()){
                 cnt+=mpp[sum-k];
+            }
             mpp[sum]++;
         }
         return cnt;
-
     }
 };
