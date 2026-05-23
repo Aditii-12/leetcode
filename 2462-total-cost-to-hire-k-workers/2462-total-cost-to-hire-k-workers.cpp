2b98e1e7ef5dca44fb77df9faf5f25f4e92dc1c3
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-05-23 13:47:35 +0530

    Time: 121 ms (7.99%), Space: 79.6 MB (32.48%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -2,33 +2,33 @@ class Solution {
 public:
     long long totalCost(vector<int>& costs, int k, int candidates) {
         int n=costs.size();
-        int i=0;
-        int j=n-1;
-        priority_queue<int,vector<int>,greater<int>>p; //min heap
-        priority_queue<int,vector<int>,greater<int>>q;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
+        int l=0;
+        int r=n-1;
+        for(int i=0;i<candidates && l<=r;i++){ //filling from left
+            pq.push({costs[l],l});
+            l++;
+        }
+        for(int i=0;i<candidates && l<=r;i++){
+            pq.push({costs[r],r});
+            r--;
+        }
         long long ans=0;
         while(k--){
-            while(p.size()<candidates && i<=j) {
-                p.push(costs[i]);
-                i++;
-            }
-            while(q.size()<candidates && i<=j){
-                q.push(costs[j]);
-                j--;
-            }
-            int t1=p.empty()?INT_MAX:p.top();
-            int t2=q.empty()?INT_MAX:q.top(); //kyuki ho skta h ek heap khali hojaye 
-            //but dusra rhega to empty wala case we will check
-            //jese 4 h total costs but candidares is 3
-            //to p me 3 q me 1 kyuki common ni
-            // or fir q wala hi choose hogya to q empty hojayega
-            if(t1<=t2){
-                ans+=t1;
-                p.pop();
-            }
-            else{
-                ans+=t2;
-                q.pop();
+            auto curr=pq.top();
+            pq.pop();
+            int cost=curr.first;
+            int ind=curr.second;
+            ans+=cost;
+            if(l<=r){
+                if(ind<l){
+                    pq.push({costs[l],l});
+                    l++;
+                }
+                else{
+                    pq.push({costs[r],r});
+                    r--;
+                }
             }
         }
         return ans;
