7a3658594d6916e35047c8626147cb0db45a2be5
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-06-02 15:16:50 +0530

    Time: 4 ms (52.15%), Space: 95.3 MB (63.75%) - LeetHub

---
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans=0;
+    void dfs(TreeNode* root,int dir,int len){
+        if(!root) return;
+        ans=max(ans,len);
+        if(dir==0){
+            dfs(root->right,1,len+1);
+            dfs(root->left,0,1);
+        }
+        else{
+            dfs(root->left,0,len+1);
+            dfs(root->right,1,1);
+        }
+    }
+    int longestZigZag(TreeNode* root) {
+        dfs(root->left,0,1);
+        dfs(root->right,1,1);
+        return ans;
+    }
+};
