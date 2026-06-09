ac61a4db4ea13691a254947ccb012256d5e72d7a
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-06-09 12:10:53 +0530

    Time: 1 ms (39.83%), Space: 13.4 MB (45.03%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,47 +1,42 @@
 class Solution {
 public:
-    vector<int> parent;
-    vector<int> rank;
+vector<int>par;
+vector<int>rank;
     int find(int x){
-        if(x==parent[x]) return x;
-        else return parent[x]=find(parent[x]);
+        if(par[x]==x) return x;
+        return par[x]=find(par[x]);
     }
     void unionn(int x,int y){
-        int x_p=find(x);
-        int y_p=find(y);
-        if(x_p==y_p) return;
+        int u=find(x);
+        int v=find(y);
+        if(u==v) return;
+        if(rank[u]<rank[v]){
+            par[u]=v;
+        }
+        else if(rank[v]<rank[u]){
+            par[v]=u;
+        }
         else{
-            if(rank[x_p]>rank[y_p]){
-                parent[y_p]=x_p;
-            }
-            else if(rank[x_p] < rank[y_p]){
-            parent[x_p] = y_p;
-        }
-        else {
-            parent[x_p] = y_p;
-            rank[y_p]++;
-        }
+            par[u]=v;
+            rank[v]++;
         }
     }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        //no of edges == no of nodes
-        //kyuki bs ek edge extra added h tree h
-        //to generally no of nodes=edges-1;
-        //but we r given one xtra edge. so no of edges given==no of nodes
-        int m=edges.size();
-        parent.resize(m+1); //1-indexed
-        rank.resize(m+1,0);
-        for(int i=1;i<m+1;i++){
-            parent[i]=i;
+        int n=edges.size();
+        par.resize(n+1);
+        rank.resize(n+1,0);
+        for(int i=1;i<n+1;i++){
+            par[i]=i;
         }
-        for(int i=0;i<m;i++){
-            int x=edges[i][0];
-            int y=edges[i][1];
-            int x_p=find(x);
-            int y_p=find(y);
-            if(x_p==y_p) return{x,y};
-            else unionn(x, y);
+        for(auto e:edges){
+            int u=e[0];
+            int v=e[1];
+            if(find(u)==find(v)) return {e[0],e[1]};
+            else{
+                unionn(e[0],e[1]);
+            }
         }
-        return{0,0};
+        return {};
+
     }
 };
