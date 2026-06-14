47d1ad9e0e5261b2cbe359802532cb330c37a38c
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-06-14 18:23:08 +0530

    Time: 424 ms (51.17%), Space: 47.6 MB (53.05%) - LeetHub

---
+++ 3766-minimum-operations-to-make-binary-palindrome/3766-minimum-operations-to-make-binary-palindrome.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isp(int x){
+        if(x==0) return 1;
+        string s;
+        while(x){
+            if(x&1) s+=(1+'0');
+            else s+=(0+'0');
+            x>>=1;
+        }
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            if(s[i]!=s[j]) return 0;
+            i++;
+            j--;
+        }
+        return 1;
+    }
+    vector<int> minOperations(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>p;
+        for(int i=0;i<=5000;i++){
+            if(isp(i)) p.push_back(i);
+        }
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            int b=INT_MAX;
+            for(auto it:p){
+                b=min(b,abs(it-nums[i]));
+            }
+            ans.push_back(b);
+        }
+        return ans;
+    }
+};
