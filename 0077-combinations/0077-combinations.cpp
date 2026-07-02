7585d0437834272716341f7cacab83e5a1af5907
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-07-02 11:30:45 +0530

    Time: 71 ms (49.7%), Space: 82.4 MB (55.05%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -2,23 +2,18 @@ class Solution {
 public:
     vector<vector<int>>ans;
     vector<int>path;
-    void func(int idx,vector<int>&p,int k){
+    void func(int idx,int k,int n){
         if(path.size()==k) {
             ans.push_back(path);
             return;}
-        int n=p.size();
-        for(int i=idx;i<n;i++){
-            path.push_back(p[i]);
-            func(i+1,p,k);
+        for(int i=idx;i<=n;i++){
+            path.push_back(i);
+            func(i+1,k,n);
             path.pop_back();
         }
     }
     vector<vector<int>> combine(int n, int k) {
-        vector<int>p(n);
-        for(int i=1;i<=n;i++){
-            p[i-1]=i;
-        }
-        func(0,p,k);
+        func(1,k,n);
         return ans;
     }
 };
