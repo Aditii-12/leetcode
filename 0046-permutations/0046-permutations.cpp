1519948c370d559c6879e8cbdc438c2953408480
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-07-02 14:22:44 +0530

    Time: 0 ms (100%), Space: 11 MB (20.71%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>>ans;
+    vector<int>path;
+    vector<bool>used;
+    void func(vector<int>& nums){
+        if(path.size()==nums.size()){
+            ans.push_back(path);
+            return;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(used[i]==1) continue;
+            path.push_back(nums[i]);
+            used[i]=1;
+            func(nums);
+            used[i]=0;
+            path.pop_back();
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        used.resize(nums.size());
+        func(nums);
+        return ans;
+    }
+};
