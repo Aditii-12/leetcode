dbe95ae891c202c8244d7b9c9cd0679d09ce5343
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-07-04 17:46:54 +0530

    Time: 19 ms (10.09%), Space: 34.9 MB (88.49%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
-    bool func(vector<int>& wts,int days,int mid){
-        int n=wts.size();
-        int temp=0;
-        int t=0;
-        for(int i=0;i<n;i++){
-            t+=wts[i];
-            if(t>mid){
-                temp++;
-                t=wts[i];
-            }
-            else if(t==mid){
-                temp++;
-                t=0;
+    bool func(int mid,vector<int>&w,int days){
+        int d=0;
+        int sum=0;
+        for(int i=0;i<w.size();i++){
+            sum+=w[i];
+            if(sum>mid){
+                d++;
+                sum=w[i];
             }
         }
-        if(t>0) temp++;
-        return temp<=days;
-
+        if(sum>0) d++;
+        return d<=days;
     }
-    int shipWithinDays(vector<int>& wts, int days) {
-        int n=wts.size();
-        int lo=*max_element(wts.begin(),wts.end());
-        int hi=accumulate(wts.begin(),wts.end(),0);
-        int ans;
-        while(lo<=hi){
-            int mid=lo+(hi-lo)/2;
-            if(func(wts,days,mid)){
-                ans=mid;
-                hi=mid-1;
+    int shipWithinDays(vector<int>& w, int days) {
+        int l=*max_element(w.begin(),w.end());
+        int h=accumulate(w.begin(),w.end(),0);
+        int ans=0;
+        while(l<=h){
+            int m=l+(h-l)/2;
+            if(func(m,w,days)){
+                ans=m;
+                h=m-1;
             }
-            else lo=mid+1;
+            else{
+                l=m+1;
+            }
         }
         return ans;
     }
