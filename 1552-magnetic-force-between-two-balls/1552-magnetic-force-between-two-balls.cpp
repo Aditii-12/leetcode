4d3610d3f0603469c5dcc204a7ffbd86580ff934
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-07-04 18:48:40 +0530

    Time: 47 ms (68.97%), Space: 61.7 MB (69.61%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    bool func(int mid,vector<int>&pos,int m){
-        int pre=pos[0]; //pehla ball rkhenge
-        int cnt=1; //we need to check ki sare balls place kiye ya ni 
+    bool func(int mid,vector<int>& pos,int m){
+        int pre=pos[0];
+        int cnt=1;
         for(int i=1;i<pos.size();i++){
             int curr=pos[i];
-            if(curr-pre>=mid) 
-            {cnt++;
-            pre=pos[i];
+            if(curr-pre>=mid){
+                cnt++;
+                pre=curr;
             }
             if(cnt==m) break;
         }
         return cnt==m;
     }
-    int maxDistance(vector<int>& position, int m) {
-        int n=position.size();
-        sort(position.begin(),position.end());
+    int maxDistance(vector<int>& pos, int m) {
+        int n=pos.size();
+        sort(pos.begin(),pos.end());
         int l=1;
-        int r=position[n-1]-position[0];
+        int r=pos[n-1]-pos[0];
         int ans=l;
         while(l<=r){
             int mid=l+(r-l)/2;
-            if(func(mid,position,m)) {
+            if(func(mid,pos,m)){
                 ans=mid;
-                l=mid+1; //ab or bda dhudenge
+                l=mid+1;
             }
             else r=mid-1;
         }
