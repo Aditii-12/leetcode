29d68a5b2d873c420e4af77b35e56656e8480954
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-07-05 18:20:57 +0530

    Time: 20 ms (93.76%), Space: 88.5 MB (51.06%) - LeetHub

--- 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,46 +1,23 @@
 class Solution {
 public:
     bool func(int mid,vector<int>& quantities,int n){
-        // int j=0;
-        // int rem=quantities[j];
-        // //max we can give to a store is mid
-        // for(int i=0;i<n;i++){
-        //     if(rem<=mid){
-        //         j++;
-        //         if(j==quantities.size()) return 1; 
-        //         //end me aagye and sare baat diya 
-        //         else{
-        //             rem=quantities[j]; //set to next product
-        //         }
-        //     }
-        //     else{
-        //         rem-=mid;
-        //     }
-        // }
-        // return 0;
-
-        //SECOND APPROACH IS(OTIMIZED ONE)
-        //IS THAT FOR A MAX QUANITY WHICH IS PASSED IN A FUCNTION
-        //WE CALCULATE NUMBER OF STORES WE NEED
-        //IF THAT<=N RETURN 1;
-        //ELSE RETURN 0
         if(mid==0) return 0;
         int temp=0;
         for(int i=0;i<quantities.size();i++){
-            temp+=ceil((double)quantities[i]/mid);
+            if(quantities[i]%mid==0){
+                temp+=quantities[i]/mid;
+            }
+            else temp+=quantities[i]/mid+1;
         }
         return temp<=n;
     }
     int minimizedMaximum(int n, vector<int>& quantities) {
         int l=1;
         int h=*max_element(quantities.begin(),quantities.end());
-        //max is jb highest quantity wala item pura ek hi store ko dediya
         int ans=-1;
         while(l<=h){
-            
             int m=l+(h-l)/2;
-            if(func(m,quantities,n)){ //agr baat skte h to then we search for a smaller
-            //answer
+            if(func(m,quantities,n)){ 
                 ans=m;
                 h=m-1; 
             }
