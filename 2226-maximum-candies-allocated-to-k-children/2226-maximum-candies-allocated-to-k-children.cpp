11d3372f9b964794d2434124992d72c63fd0f324
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-07-05 18:32:36 +0530

    Time: 27 ms (54%), Space: 88.4 MB (38.1%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     bool func(vector<int>& candies,long long k,int mid){
         long long c=0;
-        //we will see kitne bcho me we can distribute mid candies
-        //in the dekhlenge c>=n or not
         for(int i=0;i<candies.size();i++){
             c+=candies[i]/mid;
         }
@@ -15,10 +13,10 @@ public:
         int h=*max_element(candies.begin(),candies.end());
         while(l<=h){
             int m=l+(h-l)/2;
-            if(func(candies,k,m)) //possible h m candies baatna among k bche
+            if(func(candies,k,m)) 
             {
                 ans=m;
-                l=m+1; //now zada candies baatkr dekhenge
+                l=m+1;
             }
             else h=m-1;
         }
