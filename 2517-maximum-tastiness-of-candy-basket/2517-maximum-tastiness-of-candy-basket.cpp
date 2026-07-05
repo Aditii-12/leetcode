25b1a4e00a35947526536c790c43614bd1ef95d0
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-07-05 19:18:48 +0530

    Time: 57 ms (18.9%), Space: 52.2 MB (60.19%) - LeetHub

--- 2517-maximum-tastiness-of-candy-basket/2517-maximum-tastiness-of-candy-basket.cpp
+++ 2517-maximum-tastiness-of-candy-basket/2517-maximum-tastiness-of-candy-basket.cpp
@@ -4,26 +4,27 @@ public:
         int cnt=1;
         int prev=price[0];
         for(int i=1;i<price.size();i++){
-            if(price[i]-prev>=mid){
+            if(price[i]-prev>=mid) {
                 cnt++;
                 prev=price[i];
-            }        
+            }
         }
         return cnt>=k;
     }
     int maximumTastiness(vector<int>& price, int k) {
         sort(price.begin(),price.end());
+        int n=price.size();
         int l=0;
+        int h=price[n-1]-price[0];
         int ans=0;
-        int r=price[price.size()-1]- price[0];
-        while(l<=r){
-            int mid=l+(r-l)/2;
+        while(l<=h){
+            int mid=l+(h-l)/2;
             if(func(mid,price,k)){
                 ans=mid;
                 l=mid+1;
             }
-            else{
-                r=mid-1;
+            else {
+                h=mid-1;
             }
         }
         return ans;
