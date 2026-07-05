c15ec33220cd585ef9cb01a6630001d970d01245
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-07-05 18:08:03 +0530

    Time: 27 ms (80.33%), Space: 59.7 MB (99.02%) - LeetHub

--- 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.cpp
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.cpp
@@ -9,19 +9,21 @@ public:
         }
         if(totalop<=maxop) return 1;
         else return 0;
+
     }
     int minimumSize(vector<int>& nums, int maxop) {
         int n=nums.size();
         int l=1;
-        int r=*max_element(nums.begin(),nums.end());
-        int ans=r;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(func(nums,maxop,mid)) {ans=mid;
-            r=mid-1;}
+        int h=*max_element(nums.begin(),nums.end());
+        int ans=h;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(func(nums,maxop,mid)){
+                ans=mid;
+                h=mid-1;
+            }
             else l=mid+1;
         }
         return ans;
-
     }
 };
