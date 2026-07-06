f1d24bf8143085b49f325b6d081e9b915ef64d62
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-07-06 12:42:50 +0530

    Time: 100 ms (58.7%), Space: 82.9 MB (75.78%) - LeetHub

--- 1898-maximum-number-of-removable-characters/1898-maximum-number-of-removable-characters.cpp
+++ 1898-maximum-number-of-removable-characters/1898-maximum-number-of-removable-characters.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    bool func(string s,string p,vector<int>& removable,int k){
+    bool func(int mid,string &s,string &p,vector<int>& removable){
         vector<bool>removed(s.size(),0);
-        for(int i=0;i<k;i++){
+        for(int i=0;i<mid;i++){
             removed[removable[i]]=1;
         }
         int j=0; //for string p pointer
@@ -13,17 +13,18 @@ public:
         return j==p.size();
     }
     int maximumRemovals(string s, string p, vector<int>& removable) {
+        int k=removable.size();
         int l=0;
-        int r=removable.size();
-        int ans=l;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(func(s,p,removable,mid)){
+        int h=k;
+        int ans=0;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(func(mid,s,p,removable)){
                 ans=mid;
                 l=mid+1;
             }
-            else r=mid-1;
+            else h=mid-1;
         }
         return ans;
-}
+    }
 };
