ac8fbd522ff9236967ac13920dab986f2bd10b71
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-07-10 10:39:58 +0530

    Time: 29 ms (25.54%), Space: 102.9 MB (64.1%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-//next greater element kitne elements bd ayega
-//monotonic decreasing stack
     vector<int> dailyTemperatures(vector<int>& temp) {
         int n=temp.size();
-        stack<int>st;
         vector<int>ans(n,0);
+        stack<int>st;
         for(int i=n-1;i>=0;i--){
             while(!st.empty() && temp[st.top()]<=temp[i]){
                 st.pop();
             }
-            //ab jo top pr bcha h voi next greater temp h to bs distance lelnge
-            if(!st.empty())
+            if(!st.empty()){
                 ans[i]=st.top()-i;
+            }
             st.push(i);
         }
         return ans;
-    
     }
 };
