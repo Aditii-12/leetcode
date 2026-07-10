0e4c5b8a79b796f17e5003997dae7e4fdfac3630
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-07-10 13:18:41 +0530

    Time: 0 ms (100%), Space: 8.9 MB (16.72%) - LeetHub

--- 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     string smallestSubsequence(string s) {
-        vector<int>freq(26,0);
-        for(auto it:s){
-            freq[it-'a']++;
+        int n=s.size();
+        vector<int>last(26);
+        vector<bool>vis(26,0);
+        for(int i=0;i<n;i++){
+            last[s[i]-'a']=i;
         }
-        vector<bool>inst(26,0);
-        stack<int>st;
-        for(auto it:s){
-            freq[it-'a']--;
-            if(inst[it-'a']) continue;
-            while(!st.empty() && st.top()>it && freq[st.top()-'a']>0){
-                inst[st.top()-'a']=0;
+        stack<char>st;
+        for(int i=0;i<s.size();i++){
+            char ch=s[i];
+            if(vis[ch-'a']) continue;
+            while(!st.empty() && st.top()>s[i] && last[st.top()-'a']>i){
+                vis[st.top()-'a']=0;
                 st.pop();
             }
-            st.push(it);
-            inst[it-'a']=1;
+            st.push(ch);
+            vis[ch-'a']=1;
         }
-        string ans;
-        while(!st.empty()) {
+        string ans="";
+        while(!st.empty()){
             ans+=st.top();
             st.pop();
         }
-        reverse(ans.begin(), ans.end());
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
