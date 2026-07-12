b662e53f648127ba047ecc36e6ae4cab0f8bcd73
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-07-12 15:46:19 +0530

    Time: 19 ms (30.2%), Space: 27.5 MB (15.74%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
@@ -12,21 +12,24 @@
 class Solution {
 public:
     vector<TreeNode*>ans;
-    unordered_set<int>todel;
-    TreeNode* dfs(TreeNode* root,vector<int>& todlt){
-        if(!root) return NULL; 
-        root->left=dfs(root->left,todlt);
-        root->right=dfs(root->right,todlt);
-        if(todel.count(root->val)){
+    unordered_set<int>st;
+    TreeNode* dfs(TreeNode* root,vector<int>& to_del){
+        if(!root) return NULL;
+        root->left=dfs(root->left,to_del);
+        root->right=dfs(root->right,to_del);
+        if(st.count(root->val)){
             if(root->left) ans.push_back(root->left);
             if(root->right) ans.push_back(root->right);
             return NULL;
         }
         return root;
+
     }
-    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
-        for(int x:to_delete) todel.insert(x);
-        root=dfs(root,to_delete);
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_del) {
+        for(auto x:to_del){
+            st.insert(x);
+        }
+        root=dfs(root,to_del);
         if(root) ans.push_back(root);
         return ans;
     }
