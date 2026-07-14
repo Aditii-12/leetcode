e3f587184b21666d2171c22c18708ba9a44ce826
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-07-14 11:00:13 +0530

    Time: 0 ms (100%), Space: 15.3 MB (94.3%) - LeetHub

--- 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.cpp
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.cpp
@@ -14,10 +14,14 @@ public:
     bool func(TreeNode* root1, TreeNode* root2){
         if(!root1 && !root2) return 1;
         if(!root1 || !root2) return 0;
-        if(root1->val!=root2->val) return 0;
-        bool normal=func(root1->left,root2->left) && func(root1->right,root2->right);
-        bool flip=func(root1->left,root2->right) && func(root1->right,root2->left);
-        return normal||flip;
+        if(root1->val != root2->val) return 0;
+        return normal(root1,root2) || flipped(root1,root2);
+    }
+    bool flipped(TreeNode* root1, TreeNode* root2){
+        return func(root1->left,root2->right) && func(root1->right,root2->left);
+    }
+    bool normal(TreeNode* root1, TreeNode* root2){
+        return func(root1->left,root2->left) && func(root1->right,root2->right);
     }
     bool flipEquiv(TreeNode* root1, TreeNode* root2) {
         return func(root1,root2);
