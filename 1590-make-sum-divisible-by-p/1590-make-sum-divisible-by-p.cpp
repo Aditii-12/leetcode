125bcf28829c33ea906e9d0745c9df89808ffd62
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-07-15 15:21:20 +0530

    Time: 82 ms (45.89%), Space: 87.7 MB (69.31%) - LeetHub

--- 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
+    using ll=long long;
     int minSubarray(vector<int>& nums, int p) {
         int n=nums.size();
+        ll sum=accumulate(nums.begin(),nums.end(),0LL);
         unordered_map<int,int>mpp;
-        long long sum=accumulate(nums.begin(),nums.end(),0LL);
-        long long rem=sum%p;
+        ll rem=sum%p;
         if(rem==0) return 0;
         mpp[0]=-1;
         int ans=INT_MAX;
-        long long pre=0;
+        sum=0;
         for(int i=0;i<n;i++){
-            pre+=nums[i];
-            int tar=(pre%p-rem+p)%p;
+            sum+=nums[i];
+            int tar=(sum%p-rem+p)%p;
             if(mpp.find(tar)!=mpp.end()){
                 ans=min(ans,i-mpp[tar]);
             }
-            mpp[pre%p]=i;
+            mpp[sum%p]=i;
         }
-        if(ans==INT_MAX || ans==n) return -1;
-        return ans;        
+        if(ans==INT_MAX||ans==n) return -1;
+        return ans;
+        
     }
 };
