c29cf76520477e9726c8be225b7518e6645651d4
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-07-20 11:43:22 +0530

    Time: 2 ms (44.68%), Space: 14.2 MB (29.27%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -2,24 +2,22 @@ class Solution {
 public:
     vector<vector<int>>ans;
     vector<int>path;
-    void func(int idx,int tar,vector<int> cand){
+    void func(int idx,vector<int>& cand,int tar){
         if(tar==0){
             ans.push_back(path);
             return;
         }
-        if(tar<0) return;
-        int n=cand.size();
-        for(int i=idx;i<n;i++){
+        for(int i=idx;i<cand.size();i++){
             if(i>idx && cand[i]==cand[i-1]) continue;
             if(cand[i]>tar) break;
             path.push_back(cand[i]);
-            func(i+1,tar-cand[i],cand);
+            func(i+1,cand,tar-cand[i]);
             path.pop_back();
         }
     }
     vector<vector<int>> combinationSum2(vector<int>& cand, int tar) {
         sort(cand.begin(),cand.end());
-        func(0,tar,cand);
+        func(0,cand,tar);
         return ans;
     }
 };
