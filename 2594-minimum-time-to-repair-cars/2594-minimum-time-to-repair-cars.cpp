7e9959b3907d5bab5a368adcfc75ec424e600a32
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-07-23 15:23:09 +0530

    Time: 53 ms (51.68%), Space: 59.4 MB (20.32%) - LeetHub

--- 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.cpp
+++ 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    using ll=long long;
-    bool func(ll mid,vector<int>& ranks,int car){
-        ll t=0;
-        for(auto it:ranks){
-            t+=(sqrt(mid/it));
+using ll=long long;
+    bool func(ll mid,vector<int>& ranks,int cars){
+        ll tot=0;
+        for(int i=0;i<ranks.size();i++){
+            tot+=(sqrt(mid/ranks[i]));
         }
-        return t>=car;
+        return tot>=cars;
     }
     long long repairCars(vector<int>& ranks, int cars) {
         sort(ranks.begin(),ranks.end());
-        ll l=1;
-        ll h=(ll)*min_element(ranks.begin(),ranks.end())*cars*cars;
-        ll ans=h;
-        while(l<=h){
-            ll mid=l+(h-l)/2;
+        ll low=1;
+        ll high=(ll)ranks[0]*cars*cars;
+        ll ans=high;
+        while(low<=high){
+            ll mid=low+(high-low)/2;
             if(func(mid,ranks,cars)){
                 ans=mid;
-                h=mid-1;
+                high=mid-1;
             }
-            else l=mid+1;
+            else low=mid+1;
         }
         return ans;
-
     }
 };
