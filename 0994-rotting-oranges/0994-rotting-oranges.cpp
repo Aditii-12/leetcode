1ce5eaa71a7521c81183fc52c1d2988f9cfc97e2
Aditi Sahu <145634635+Aditii-12@users.noreply.github.com>
2026-08-07 11:52:28 +0530

    Time: 0 ms (100%), Space: 16.8 MB (70.33%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,43 +1,45 @@
 class Solution {
 public:
+    int dr[4]={0,0,-1,1};
+    int dc[4]={1,-1,0,0};
     int orangesRotting(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
+        int m=grid.size();
+        int n=grid[0].size();
         queue<pair<int,int>>q;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==2) q.push({i,j});
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==2){
+                    q.push({i,j});
+                }
             }
         }
-        int ans=0;
-        int dr[]={-1,1,0,0};
-        int dc[]={0,0,-1,1};
-
+        int time=0;
         while(!q.empty()){
-            int size=q.size();
-            bool f=0; //to check if there is any orange rotting at that min
-            for(int i=0;i<size;i++){
-                int row=q.front().first;
-                int col=q.front().second;
+            bool f=0;
+            int sz=q.size();
+            for(int i=0;i<sz;i++){
+                auto it=q.front();
+                auto r=it.first;
+                auto c=it.second;
                 q.pop();
-                for(int i=0;i<4;i++){
-                    int nr=row+dr[i];
-                    int nc=col+dc[i];
-                    if(nr>=0&&nr<n && nc>=0 && nc<m && grid[nr][nc]==1){
-                        q.push({nr,nc});
+                for(int j=0;j<4;j++){
+                    int nr=r+dr[j];
+                    int nc=c+dc[j];
+                    if(nr<0 || nc<0 || nr>=m || nc>=n) continue;
+                    if(grid[nr][nc]==1) {
                         grid[nr][nc]=2;
+                        q.push({nr,nc});
                         f=1;
                     }
                 }
             }
-            if (f==1) ans++;
+            if(f==1) time++;
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j]==1) return -1;
             }
         }
-        return ans;
-
+        return time;
     }
 };
